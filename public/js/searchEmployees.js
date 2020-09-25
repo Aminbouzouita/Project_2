6bd24d96636deb0b1f97cd2764ead019ba213287
Harold Zuluaga <haroldzuluaga@aol.com>
2020-09-24 17:52:21 -0700

    updated search

--- public/js/searchEmployees.js
+++ public/js/searchEmployees.js
@@ -1,23 +1,86 @@
 $(document).ready(function () {
-  get("/api/allEmployees/manager", function (req, res) {
-    console.log(res);
-  //   $.get("/api/Employees/category/:category", function (data) {
-  //   //   for (var i = 0; i < data.length; i++) {
-  //   //     var departments = $("<div>").addClass("departments").attr("id", "department" + i);;
-  //   //     $("#departments-section").append(departments);
-
-  //   //     $("#department" + i).append(`Department Name:<input id=value${i} value=` + data[i].department_name + `></br>`);
-  //   //     $("#department" + i).append(`<button class=delete_btn id=${i}>Delete</button>`);
-  //   //     $("#department" + i).append(`<button  class=edit_btn id=${i}>Edit</button></br></br>`);
-  //   //   }
-
-  //   $.ajax({
-  //     method: "post",
-  //     url: "/api/Employees/category/" + `manager_id`,
-  //     data: `true`,
-  //   }).then(function (result) {
-  //     console.log(result);
-  //   });
-  //   location.reload();
+  $.get("/api/user_data").then(function (data) {
+    $(".member-name").text(data.email);
   });
-    }); 
+  // Make a get request to our api route that will return every book
+  // When user clicks add-btn
+  $("#add-btn").on("click", function (event) {
+    event.preventDefault();
+    //console.log(event);
+    var user_entry = $("#user-entry").val().trim();
+    var selected_option = $("#role_choices").val();
+    //console.log(selected_option);
+    // Make a newBook object
+    if (selected_option === "1") {
+      var newEmployee = {
+        first_name: $("#user-entry").val().trim(),
+        // last_name: $("#last_name").val().trim(),
+        // title: $("#title").val().trim(),
+        // email: $("#email").val().trim(),
+      };
+      $.get("/api/searchemployee/", newEmployee).then(function (data) {
+        // Log the data we found
+        // console.log(
+        //   "tercero",
+        //   data.id,
+        //   data.first_name,
+        //   data.email,
+        //   data.role_id,
+        //   data
+        // );
+        var employees = $("<div>").addClass("employees");
+        $(".answer-search").append(employees);
+        $(".answer-search").append(
+          `first name:<input id=first_name value=` + data.first_name + `></br>`
+        );
+        $(`.answer-search`).append(
+          `last name:<input id=last_name value= ` + data.last_name + `></br>`
+        );
+        $(`.answer-search`).append(
+          `title:<input id=role_id value= ` + data.role_id + `></br>`
+        );
+        $(`.answer-search`).append(
+          `email:<input id=email value= ` + data.email + `></br>`
+        );
+      });
+    } else if (selected_option === "2") {
+      var newEmployee = {
+        //first_name: $("#user-entry").val().trim(),
+        last_name: $("#user-entry").val().trim(),
+        // title: $("#title").val().trim(),
+        // email: $("#email").val().trim(),
+      };
+      $.get("/api/searchemployeelast/", newEmployee).then(function (data) {
+        //Log the data we found
+        // console.log(
+        //   "tercero",
+        //   data.id,
+        //   data.first_name,
+        //   data.email,
+        //   data.role_id,
+        //   data
+        // );
+        var employees = $("<div>").addClass("employees");
+        $(".answer-search").append(employees);
+        $(".answer-search").append(
+          `first name:<input id=first_name value=` + data.first_name + `></br>`
+        );
+        $(`.answer-search`).append(
+          `last name:<input id=last_name value= ` + data.last_name + `></br>`
+        );
+        $(`.answer-search`).append(
+          `title:<input id=role_id value= ` + data.role_id + `></br>`
+        );
+        $(`.answer-search`).append(
+          `email:<input id=email value= ` + data.email + `></br>`
+        );
+      });
+    }
+    // Send an AJAX POST-request with jQuery
+    // Empty each input box by replacing the value with an empty string
+    $("#first_name").val("");
+    // $("#last_name").val("");
+    // $("#title").val("");
+    // $("#email").val("");
+  });
+});
--- routes/employees-api-routes.js
+++ routes/employees-api-routes.js
@@ -7,10 +7,36 @@ module.exports = function (app) {
   app.get("/api/allEmployees", function (req, res) {
     db.Employee.findAll({
       where: req.params.id,
-      include: [db.Role]
+      include: [db.Role],
     }).then(function (results) {
       res.json(results);
-      console.log(results);
+      //console.log(results);
+    });
+  });
+
+  app.get("/api/searchemployee/?", function (req, res) {
+    var first = req.query.first_name;
+    console.log("employee one search: primero", first);
+    db.Employee.findOne({
+      where: {
+        first_name: first,
+      },
+    }).then(function (results) {
+      res.json(results);
+      return results;
+    });
+  });
+
+  app.get("/api/searchemployeelast/?", function (req, res) {
+    var last = req.query.last_name;
+    console.log("employee one search: primero", last);
+    db.Employee.findOne({
+      where: {
+        last_name: last,
+      },
+    }).then(function (results) {
+      res.json(results);
+      return results;
     });
   });
 
@@ -25,7 +51,22 @@ module.exports = function (app) {
   // });
 
   app.put("/api/updateEmp/", function (req, res) {
-    const { id, first_name, last_name, title, hourly_paid, email, street, city, zip_code, country, is_manager, manager_id, is_admin, is_active} = req.body;
+    const {
+      id,
+      first_name,
+      last_name,
+      title,
+      hourly_paid,
+      email,
+      street,
+      city,
+      zip_code,
+      country,
+      is_manager,
+      manager_id,
+      is_admin,
+      is_active,
+    } = req.body;
 
     db.Employee.update(
       {
@@ -52,8 +93,7 @@ module.exports = function (app) {
       res.json(result);
     });
   });
-  
-  
+
   app.post("/api/newEmployees", function (req, res) {
     console.log("Employee Data:");
     console.log(req.body);
@@ -61,7 +101,7 @@ module.exports = function (app) {
       first_name: req.body.first_name,
       last_name: req.body.last_name,
       role_id: req.body.role_id,
-      hourly_paid:req.body.hourly_paid,
+      hourly_paid: req.body.hourly_paid,
       email: req.body.email,
       street: req.body.street,
       city: req.body.city,
@@ -73,7 +113,6 @@ module.exports = function (app) {
       is_active: req.body.is_active,
     }).then(function (results) {
       res.json(results);
-
     });
   });
 
@@ -82,8 +121,8 @@ module.exports = function (app) {
     console.log(req.params.id);
     db.Employee.destroy({
       where: {
-        id: req.params.id
-      }
+        id: req.params.id,
+      },
     }).then(function () {
       res.end();
     });
